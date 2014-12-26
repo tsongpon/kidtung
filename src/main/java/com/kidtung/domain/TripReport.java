448f1afae359e58219cc79326a5fee761e29946d
dekaoun <ni@Supannikars-MacBook-Pro.local>
2014-12-26 13:53:12 +0700

    Implement Report endpoint

---
+++ src/main/java/com/kidtung/domain/TripReport.java
@@ -0,0 +1,31 @@
+package com.kidtung.domain;
+
+public class TripReport {
+    int memberNo;
+    Double total;
+    Double average;
+
+    public int getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getAverage() {
+        return average;
+    }
+
+    public void setAverage(Double average) {
+        this.average = average;
+    }
+}
