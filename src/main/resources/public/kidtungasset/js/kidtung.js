7a70864b1a3df8db5436fe5505d863795ee3094f
pang <pang@abctech-thailand.com>
2015-01-30 16:39:21 +0700

    Add more label color

--- src/main/resources/public/kidtungasset/js/kidtung.js
+++ src/main/resources/public/kidtungasset/js/kidtung.js
@@ -211,6 +211,6 @@ var initSummary = function ($scope, $http) {
         }).
         error(function (data, status, headers, config) {
             // called asynchronously if an error occurs
-            // or server returns response with an error status.
+            // or server returns respognse with an error status.
         });
 };
